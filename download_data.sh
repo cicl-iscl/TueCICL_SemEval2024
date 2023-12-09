48aef4fcaa08849bcdeb12436af41ee9834bb87c
waron97 <winkler.aron5gmail.com>
2023-12-09 22:55:21 +0100

    add download data script

---
+++ download_data.sh
@@ -0,0 +1,4 @@
+mkdir data
+wget https://s3.tebi.io/winkler.stuff/subtaskC_train_mapped.jsonl -O data/subtaskC_train_mapped.jsonl
+wget https://s3.tebi.io/winkler.stuff/subtaskC_train.jsonl -O data/subtaskC_train.jsonl
+wget https://s3.tebi.io/winkler.stuff/subtaskC_dev.jsonl -O data/subtaskC_dev.jsonl
