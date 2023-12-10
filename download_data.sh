fbb98f7b5615082f1276e86311f2af3d8d5d9de8
waron97 <winkler.aron5gmail.com>
2023-12-10 11:47:33 +0100

    add perplexities to download script

--- download_data.sh
+++ download_data.sh
@@ -2,3 +2,4 @@ mkdir data
 wget https://s3.tebi.io/winkler.stuff/subtaskC_train_mapped.jsonl -O data/subtaskC_train_mapped.jsonl
 wget https://s3.tebi.io/winkler.stuff/subtaskC_train.jsonl -O data/subtaskC_train.jsonl
 wget https://s3.tebi.io/winkler.stuff/subtaskC_dev.jsonl -O data/subtaskC_dev.jsonl
+wget https://s3.tebi.io/winkler.stuff/perplexities.json -O data/perplexities.json
