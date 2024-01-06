b29a36899de6db95887f02bcb108057958e22705
waron97 <winkler.aron5gmail.com>
2024-01-06 17:40:15 +0100

    add argument support

---
+++ taskA/scripts/charclass.sh
@@ -0,0 +1,14 @@
+cd ..
+source env/bin/activate
+cd src
+
+python3 script.py train_char_classifier \
+    --char-class-checkpoint-prefix char-class-256-3 \
+    --char-class-num-layers 3 \
+    --char-class-hidden-size 256 \
+    --char-class-save-every 2000 \
+    --char-class-emb-size 8 \
+    --char-class-lr 0.001 \
+    --char-class-n-epochs 5 \
+    --char-class-batch-size 8 \
+    --char-class-tokenizer-type uncondensed
--- taskA/src/util/checkpoints.py
+++ taskA/src/util/checkpoints.py
@@ -3,14 +3,14 @@ import os
 import torch
 
 from util.core import abspath
-from .validation import evaluate
 
 
 class ProgressTracker:
-    def __init__(self, prefix) -> None:
+    def __init__(self, prefix, evaluate_fn) -> None:
         self.progress = {}
         self.prefix = prefix
         self.basedir = abspath(__file__, "../../checkpoints")
+        self.evaluate_fn = evaluate_fn
 
         try:
             os.makedirs(f"{self.basedir}/{prefix}")
@@ -26,7 +26,7 @@ class ProgressTracker:
             except:
                 print("No previous best found")
                 self.progress["best"] = 0
-        metric = evaluate(model, dev_loader)
+        metric = self.evaluate_fn(model, dev_loader)
         is_best = metric > self.progress["best"]
         extra = {
             "metric": metric
@@ -38,7 +38,7 @@ class ProgressTracker:
         return self.progress["best"], metric
 
     def for_epoch(self, model, optimizer, epoch, dev_loader):
-        metric = evaluate(model, dev_loader)
+        metric = self.evaluate_fn(model, dev_loader)
         extra = {
             "optimizer": optimizer.state_dict(),
             "epoch": epoch,
