b97295fba9c8293deac4bb60495e900b4c3bdbca
Aron <winkler.aron5@gmail.com>
2024-01-14 15:17:06 +0100

    scripts

--- taskA/scripts/charclass.sh
+++ taskA/scripts/charclass.sh
@@ -4,14 +4,14 @@ cd src
 
 python3 script.py train_char_classifier \
     --char-class-do-train 1 \
-    --char-class-load-model char-class-256-3/epoch_5.pt \
-    --char-class-checkpoint-prefix char-class-256-3 \
+    --char-class-load-model charlm-512-3-max/epoch_2.pt \
+    --char-class-checkpoint-prefix charlm-512-3-max \
     --char-class-num-layers 3 \
-    --char-class-hidden-size 256 \
+    --char-class-hidden-size 512 \
     --char-class-save-every 2000 \
     --char-class-emb-size 8 \
     --char-class-lr 0.001 \
-    --char-class-start-epoch 6 \
+    --char-class-start-epoch 3 \
     --char-class-n-epochs 10 \
     --char-class-batch-size 8 \
     --char-class-tokenizer-type uncondensed \
--- taskA/scripts/charlm.sh
+++ taskA/scripts/charlm.sh
@@ -4,15 +4,15 @@ cd src
 
 python3 script.py train_charlm \
     --charlm-do-train 1 \
-    --charlm-checkpoint-prefix charlm-256-3-mean \
-    --charlm-aggregate-fn mean \
-    --charlm-num-layers 3 \
-    --charlm-hidden-size 256 \
+    --charlm-checkpoint-prefix charlm-512-1-last \
+    --charlm-aggregate-fn last \
+    --charlm-num-layers 1 \
+    --charlm-hidden-size 512 \
     --charlm-save-every 2000 \
     --charlm-emb-size 8 \
     --charlm-lr 0.001 \
     --charlm-n-epochs 5 \
-    --charlm-batch-size 6 \
+    --charlm-batch-size 8 \
     --charlm-tokenizer-type uncondensed \
     --charlm-window-size 4000 \
     --charlm-context-size 1000 
