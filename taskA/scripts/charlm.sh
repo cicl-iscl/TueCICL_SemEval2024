6616f158ae3f0585c4ab03c4083a8d864086327a
waron97 <winkler.aron5@gmail.com>
2024-01-15 17:53:47 +0100

    restore windows with hidden states

--- taskA/scripts/charlm.sh
+++ taskA/scripts/charlm.sh
@@ -4,15 +4,15 @@ cd src
 
 python3 script.py train_charlm \
     --charlm-do-train 1 \
-    --charlm-checkpoint-prefix charlm-256-3-last \
+    --charlm-checkpoint-prefix charlm-512-2-last \
     --charlm-aggregate-fn last \
-    --charlm-num-layers 3 \
-    --charlm-hidden-size 256 \
-    --charlm-save-every 2500 \
+    --charlm-num-layers 2 \
+    --charlm-hidden-size 512 \
+    --charlm-save-every 10000 \
     --charlm-emb-size 8 \
     --charlm-lr 0.001 \
     --charlm-n-epochs 5 \
-    --charlm-batch-size 6 \
+    --charlm-batch-size 2 \
     --charlm-tokenizer-type uncondensed \
     --charlm-dropout 0.1 \
     --charlm-tokenizer-max-len 15000
