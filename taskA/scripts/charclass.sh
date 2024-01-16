7dcd70587b8710b3d27a726824d591a6d2a1b4f5
waron97 <winkler.aron5@gmail.com>
2024-01-16 09:05:42 +0100

    script

--- taskA/scripts/charclass.sh
+++ taskA/scripts/charclass.sh
@@ -4,15 +4,15 @@ cd src
 
 python3 script.py train_char_classifier \
     --char-class-do-train 1 \
-    --char-class-checkpoint-prefix cc-512-2 \
+    --char-class-checkpoint-prefix cc-128-2 \
     --char-class-num-layers 2 \
-    --char-class-hidden-size 512 \
-    --char-class-save-every 2000 \
+    --char-class-hidden-size 128 \
+    --char-class-save-every 3000 \
     --char-class-emb-size 8 \
     --char-class-lr 0.001 \
     --char-class-start-epoch 1 \
     --char-class-n-epochs 10 \
-    --char-class-batch-size 8 \
+    --char-class-batch-size 6 \
     --char-class-tokenizer-type uncondensed \
-    --char-class-tokenizer-max-len 20000 \
-    --char-class-dropout 0.1 \
+    --char-class-tokenizer-max-len 15000 \
+    --char-class-dropout 0.1  
