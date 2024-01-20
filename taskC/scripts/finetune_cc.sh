b95dcce9931ee1f08956771471d2c2643526c301
waron97 <winkler.aron5gmail.com>
2024-01-20 17:25:01 +0100

    add finetune script

---
+++ taskC/scripts/finetune_cc.sh
@@ -0,0 +1,10 @@
+cd ..
+source env/bin/activate
+cd src
+
+python3 script.py finetune_cc \
+    --cc-labeller-tokenizer-path ~/cicl/taskC/data/charlm_vocab_uncondensed.pkl \
+    --cc-labeller-cc-path ~/cicl/taskC/data/pretrained_parameters/taska_cc.pt \
+    --cc-labeller-checkpoint-prefix cc-test \
+    --cc-labeller-tokenizer-max-len 1000 \
+    --cc-labeller-save-every 10 \
