129a1c698184e2a10cd49b66ae4f540f27ba41c9
waron97 <winkler.aron5@gmail.com>
2024-01-22 22:13:22 +0100

    Update word2vec_bilstm.sh script parameters

--- taskC/scripts/word2vec_bilstm.sh
+++ taskC/scripts/word2vec_bilstm.sh
@@ -3,17 +3,17 @@ source env/bin/activate
 cd src
 
 python3 script.py train_word2vec_bilstm \
-    --word2vec-bilstm-tokenizer-path ~/cicl/taskC/data/enwiki_20180420_500d.txt \
-    --word2vec-bilstm-save-vocab ~/cicl/taskC/data/wiki2vec_vocab_500.pkl \
-    --word2vec-bilstm-checkpoint-prefix word2vec-bilstm-test \
+    --word2vec-bilstm-tokenizer-vocab ~/cicl/taskC/data/wiki2vec_vocab_500.pkl \
+    --word2vec-bilstm-tokenizer-weights ~/cicl/taskC/data/wiki2vec_weights_500.pkl \
+    --word2vec-bilstm-checkpoint-prefix word2vec-bilstm-256-2-pure-only \
     --word2vec-bilstm-tokenizer-max-len 15000 \
-    --word2vec-bilstm-save-every-extended 10 \
-    --word2vec-bilstm-save-every-pure 10 \
-    --word2vec-bilstm-epochs-extended 1 \
-    --word2vec-bilstm-epochs-pure 1 \
+    --word2vec-bilstm-epochs-extended 0 \
+    --word2vec-bilstm-save-every-extended 3000 \
+    --word2vec-bilstm-epochs-pure 50 \
+    --word2vec-bilstm-save-every-pure 250 \
     --word2vec-bilstm-hidden-size 256 \
-    --word2vec-bilstm-emb-size 16 \
+    --word2vec-bilstm-emb-size 500 \
     --word2vec-bilstm-num-layers 2 \
     --word2vec-bilstm-dropout 0.2 \
-    --word2vec-bilstm-batch-size 4 \
+    --word2vec-bilstm-batch-size 6 \
     --word2vec-bilstm-train \
