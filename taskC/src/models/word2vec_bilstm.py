641e305517d4e73bce90ebc7b8ecbd55e3af3ea3
waron97 <winkler.aron5gmail.com>
2024-01-22 19:21:16 +0100

    Add Word2VecBiLSTM model and Word2VecTokenizer class

---
+++ taskC/src/models/word2vec_bilstm.py
@@ -0,0 +1,231 @@
+import re
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import pickle
+
+from util.device import get_device
+
+
+class Word2VecBiLSTM(nn.Module):
+    def __init__(
+        self,
+        pretrained_embeddings=None,
+        vocab_size=None,
+        emb_size=None,
+        hidden_size=256,
+        num_layers=2,
+        dropout=0.0,
+    ) -> None:
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        if pretrained_embeddings:
+            self.vocab_size = pretrained_embeddings.shape[0]
+            self.emb_size = pretrained_embeddings.shape[1]
+            self.emb = nn.Embedding.from_pretrained(pretrained_embeddings)
+        else:
+            self.vocab_size = vocab_size
+            self.emb_size = emb_size
+            self.emb = nn.Embedding(self.vocab_size, self.emb_size)
+
+        self.lstm = nn.LSTM(
+            hidden_size=self.hidden_size,
+            input_size=self.emb_size,
+            num_layers=self.num_layers,
+            batch_first=True,
+            dropout=dropout,
+            bidirectional=True
+        )
+
+        self.lstm2out = nn.Linear(hidden_size * 2, 2)
+
+        self.emb.cpu()
+        self.lstm.to(get_device())
+        self.lstm2out.to(get_device())
+
+    def __str__(self):
+        return f"Word2VecBiLSTM(vocab_size={self.vocab_size}, emb_size={self.emb_size}, hidden_size={self.hidden_size}, num_layers={self.num_layers})"
+
+    def forward(self, X):
+        embedded = self.emb(X)
+        inputs = embedded.to(get_device())
+        lstm_out, _ = self.lstm(inputs)
+        out = self.lstm2out(lstm_out)
+        out = F.log_softmax(out, dim=-1)
+        return out
+
+    def save(self, path, extra={}):
+        cp = {
+            "state_dict": self.state_dict(),
+            "vocab_size": self.vocab_size,
+            "emb_size": self.emb_size,
+            "hidden_size": self.hidden_size,
+            "num_layers": self.num_layers,
+            "dropout": self.dropout,
+            **extra
+        }
+        torch.save(cp, path)
+
+    def from_pretrained(cls, path):
+        cp = torch.load(path)
+        model = cls(
+            vocab_size=cp["vocab_size"],
+            emb_size=cp["emb_size"],
+            hidden_size=cp["hidden_size"],
+            num_layers=cp["num_layers"],
+            dropout=cp["dropout"],
+        )
+        model.load_state_dict(cp["state_dict"])
+        return model
+
+
+class Word2VecTokenizer:
+    WHITESPACE = "<WS>"
+    NUMBER = "<NUM>"
+    PUNCT = "<P>"
+    UNK = "<UNK>"
+    PAD = "<PAD>"
+
+    def __init__(self, word2idx, idx2word, max_len=5000) -> None:
+        self.word2idx = word2idx
+        self.idx2word = idx2word
+        self.max_len = max_len
+
+    def _split(self, texts):
+        spl = [
+            text.lower().split(" ")
+            for text in texts
+        ]
+        adj = [
+            [" " if x == "" else x for x in text]
+            for text in spl
+        ]
+        return adj
+
+    def _get_subwords(self, word):
+        # separate punctuation marks
+        word = re.sub(r"([^\w\s])", r" \1 ", word)
+        subwords = word.split()
+        subwords = [" " if x == "" else x for x in subwords]
+        return subwords
+
+    def _is_punct(self, word: str):
+        return re.match(r"([^\w\s])", word)
+
+    def _is_number(self, word: str):
+        return re.match(r"^[0-9]+$", word)
+
+    def _get_id(self, word: str):
+        if word.isspace():
+            return self.word2idx[self.WHITESPACE]
+        elif self._is_punct(word):
+            return self.word2idx[self.PUNCT]
+        elif self._is_number(word):
+            return self.word2idx[self.NUMBER]
+        return self.word2idx.get(word, self.word2idx[self.UNK])
+
+    def tokenize(self, texts, label_bounds):
+        _ids = []
+        _words = []
+        _attentions = []
+        _labels = []
+
+        texts = self._split(texts)
+
+        for text_idx, text in enumerate(texts):
+            label_bound = label_bounds[text_idx]
+            text_ids = []
+            text_words = []
+            text_labels = []
+            for word_idx, word in enumerate(text):
+                subwords = self._get_subwords(word)
+                for subword in subwords:
+                    text_ids.append(self._get_id(subword))
+                    text_words.append(word_idx)
+                    text_labels.append(label_bound[word_idx])
+            _ids.append(text_ids)
+            _words.append(text_words)
+            _labels.append(text_labels)
+
+        longest = max([len(x) for x in _ids])
+        if longest > self.max_len:
+            longest = self.max_len
+
+        for i in range(len(_ids)):
+            _ids[i] = _ids[i][:longest]
+            _words[i] = _words[i][:longest]
+            _labels[i] = _labels[i][:longest]
+            l = len(_ids[i])
+            _attentions[i] = [1] * l
+            if l < longest:
+                _ids[i] += [self.word2idx[self.PAD]] * (longest - l)
+                _words[i] += [-1] * (longest - l)
+                _labels[i] += [0] * (longest - l)
+                _attentions[i] += [0] * (longest - l)
+
+        return (
+            torch.tensor(_ids),
+            torch.tensor(_words),
+            torch.tensor(_labels),
+            torch.tensor(_attentions)
+        )
+
+    def save(self, path):
+        with open(path, "wb") as f:
+            pickle.dump(self.word2idx, f)
+
+    @classmethod
+    def from_txt(cls, path, emb_size, max_len=5000):
+        word2idx = {}
+        idx2word = {}
+        weights = []
+        print("Loading word2vec from", path)
+        with open(path, "r") as f:
+            next(f)
+            i = 0
+            for line in f:
+                if not line:
+                    continue
+                spl = line.split()
+                vec = [float(x) for x in spl[-emb_size:]]
+                word = " ".join(spl[:-emb_size])
+
+                if len(word.split()) > 1:
+                    continue
+
+                word2idx[word] = i
+                idx2word[i] = word
+                weights.append(vec)
+                i += 1
+
+        for token in [cls.WHITESPACE, cls.NUMBER, cls.UNK, cls.PAD, cls.PUNCT]:
+            word2idx[token] = len(word2idx)
+            idx2word[len(idx2word)] = token
+            weights.append([0.0] * emb_size)
+
+        c = cls(word2idx, idx2word, max_len=max_len)
+        return c, torch.tensor(weights)
+
+    @classmethod
+    def from_pretrained(cls, path, max_len=5000):
+        with open(path, "rb") as f:
+            word2idx = pickle.load(f)
+        idx2word = {v: k for k, v in word2idx.items()}
+        return cls(word2idx, idx2word, max_len=max_len)
+
+    @staticmethod
+    def collate_fn(tokenizer):
+        def collate_batch(batch):
+            texts = [x[0] for x in batch]
+            true_labels = [x[1] for x in batch]
+            input_ids, words, labels, attentions = tokenizer.tokenize(
+                texts, true_labels)
+
+            for i in range(len(texts)):
+                assert true_labels[i] == labels[i].tolist().index(1)
+
+            return input_ids, words, labels, attentions
+
+        return collate_batch
---
+++ taskC/src/trainers/train_word2vec_bilstm.py
@@ -0,0 +1,213 @@
+from argparse import Namespace
+from dataclasses import dataclass
+
+import torch
+from tqdm import tqdm
+from loader.data import TaskC_Data
+from util.checkpoints import ProgressTracker
+
+from util.device import get_device
+from models.word2vec_bilstm import Word2VecBiLSTM, Word2VecTokenizer
+from torch.utils.data import DataLoader
+
+
+def add_args(parser):
+    group = parser.add_argument_group("char-bilstm")
+    def p(cmd): return f"--word2vec-bilstm-{cmd}"
+    group.add_argument(p("tokenizer-path"), type=str, default=None)
+    group.add_argument(p("tokenizer-vocab"), type=str, default=None)
+    group.add_argument(p("save-vocab"), type=str, default=None)
+    group.add_argument(p("emb-size"), type=int, default=500)
+    group.add_argument(p("tokenizer-max-len"), type=int, default=10000)
+    group.add_argument(p("batch-size"), type=int, default=8)
+    group.add_argument(p("train"), action="store_true", default=False)
+    group.add_argument(p("epochs-extended"), type=int, default=10)
+    group.add_argument(p("epochs-pure"), type=int, default=10)
+    group.add_argument(p("lr"), type=float, default=1e-3)
+    group.add_argument(p("save-every-extended"), type=int, default=1000)
+    group.add_argument(p("save-every-pure"), type=int, default=1000)
+    group.add_argument(p("hidden-size"), type=int, default=2)
+    group.add_argument(p("num-layers"), type=int, default=2)
+    group.add_argument(p("dropout"), type=float, default=0.2)
+    group.add_argument(p("checkpoint-prefix"), type=str, default="char_bilstm")
+    group.add_argument(p("load-model"), type=str, default="char_bilstm")
+
+
+def evaluate(_model, dev_loader):
+    model = _model.module
+    model.eval()
+    distances = []
+
+    def _true_label(labels, words):
+        l = labels.cpu().tolist()
+        _i = None
+        if 1 in l:
+            _i = l.index(1)
+        else:
+            _i = len(l) - 1
+        return words[_i].item()
+
+    for ids, labels, words, _ in dev_loader:
+        ids = ids.to(get_device())
+        with torch.no_grad():
+            out = model(ids)
+            preds = torch.argmax(out, dim=-1)
+            for i in range(len(preds)):
+                true_label = _true_label(labels[i], words[i])
+                true_prediction = _true_label(preds[i], words[i])
+                distances.append(abs(true_label - true_prediction))
+
+    return sum(distances) / len(distances)
+
+
+@dataclass
+class TrainingArguments:
+    model: Word2VecBiLSTM = None
+    tokenizer: Word2VecBiLSTM = None
+    epochs_extended: int = None
+    epochs_pure: int = None
+    optimizer: torch.optim.Optimizer = None
+    save_every_extended: int = None
+    save_every_pure: int = None
+    train_loader_pure: DataLoader = None
+    train_loader_extended: DataLoader = None
+    dev_loader: DataLoader = None
+    checkpoint_prefix: str = "word2vec_labeller"
+    criterion: torch.nn.Module = None
+
+
+def perform_training_step(args, batch):
+    ids, labels, _, attentions = batch
+    ids = ids.to(get_device())
+    labels = labels.to(get_device())
+    args.optimizer.zero_grad()
+
+    loss = torch.tensor(0.0, device=get_device())
+    out = args.model(ids)
+
+    for j in range(len(ids)):
+        a = attentions[j].cpu().tolist()
+        attention_bound = a.index(0) if 0 in a else len(a)
+        local_out = out[j][:attention_bound]
+        local_labels = labels[j][:attention_bound]
+        loss += args.criterion(local_out, local_labels)
+
+    loss.backward()
+    args.optimizer.step()
+    return loss.item()
+
+
+def train(args: TrainingArguments):
+    i = 0
+    pt = ProgressTracker(args.checkpoint_prefix, evaluate_fn=evaluate)
+    args.model.train()
+    losses = []
+    for epoch in range(args.epochs_extended):
+        with tqdm(total=len(args.train_loader_extended)) as pbar:
+            pbar.set_description(f"Extended epoch {epoch}")
+            for batch in args.train_loader_extended:
+                loss = perform_training_step(args, batch)
+                i += 1
+                pbar.update(1)
+                losses.append(loss)
+                if i % args.save_every_extended == 0 and i != 0:
+                    l = sum(losses) / len(losses)
+                    losses = []
+                    best, latest = pt.for_steps(args.model, args.dev_loader)
+                    pbar.set_postfix(best=best, latest=latest, loss=l)
+                    args.model.train()
+        pt.for_epoch(args.model, args.optimizer, epoch, args.dev_loader)
+        args.model.train()
+
+    for epoch in range(args.epochs_pure):
+        with tqdm(total=len(args.train_loader_pure)) as pbar:
+            pbar.set_description(f"Pure epoch {epoch}")
+            for batch in args.train_loader_pure:
+                loss = perform_training_step(args, batch)
+                i += 1
+                pbar.update(1)
+                if i % args.save_every_pure == 0 and i != 0:
+                    l = sum(losses) / len(losses)
+                    losses = []
+                    best, latest = pt.for_steps(
+                        args.model, args.dev_loader, loss=l)
+                    pbar.set_postfix(best=best, latest=latest)
+                    args.model.train()
+        _e = epoch + args.epochs_extended
+        pt.for_epoch(args.model, args.optimizer, _e, args.dev_loader)
+        args.model.train()
+
+
+def entry(args: Namespace):
+    def arg(cmd):
+        p = f"word2vec_bilstm_{cmd.replace('-', '_')}"
+        return args.__getattribute__(p)
+
+    weights = None
+    if arg("tokenizer-vocab"):
+        tokenizer = Word2VecTokenizer.from_pretrained(arg("tokenizer-vocab"))
+    elif arg("tokenizer-path"):
+        tokenizer, weights = Word2VecTokenizer.from_txt(arg("tokenizer-path"))
+        if arg("save-vocab"):
+            tokenizer.save(arg("save-vocab"))
+    else:
+        raise ValueError(
+            "Either tokenizer-path or tokenizer-vocab must be provided")
+
+    if arg("load-model"):
+        model = Word2VecBiLSTM.from_pretrained(arg("load-model"))
+    else:
+        if weights is None:
+            raise ValueError("No pretrained weights provided")
+        model = Word2VecBiLSTM(
+            pretrained_embeddings=weights,
+            hidden_size=arg("hidden-size"),
+            num_layers=arg("num-layers"),
+            dropout=arg("dropout"),
+        )
+    print(model)
+    model = torch.nn.DataParallel(model)
+    optimizer = torch.optim.Adam(model.parameters(), lr=arg("lr"))
+
+    train_ds_ext = TaskC_Data(split="train")
+    train_ds_ext.import_task_A()
+    train_ds_pure = TaskC_Data(split="train")
+    dev_ds = TaskC_Data(split="dev")
+
+    train_dl_ext = DataLoader(
+        train_ds_ext,
+        batch_size=arg("batch-size"),
+        shuffle=True,
+        collate_fn=Word2VecTokenizer.collate_fn(tokenizer),
+        drop_last=True
+    )
+    train_dl_pure = DataLoader(
+        train_ds_pure,
+        batch_size=arg("batch-size"),
+        shuffle=True,
+        collate_fn=Word2VecTokenizer.collate_fn(tokenizer),
+        drop_last=True
+    )
+    dev_dl = DataLoader(
+        dev_ds,
+        batch_size=arg("batch-size"),
+        shuffle=False,
+        collate_fn=Word2VecTokenizer.collate_fn(tokenizer)
+    )
+
+    training_arguments = TrainingArguments(
+        model=model,
+        tokenizer=tokenizer,
+        epochs_extended=arg("epochs-extended"),
+        epochs_pure=arg("epochs-pure"),
+        optimizer=optimizer,
+        save_every_extended=arg("save-every-extended"),
+        save_every_pure=arg("save-every-pure"),
+        train_loader_extended=train_dl_ext,
+        train_loader_pure=train_dl_pure,
+        dev_loader=dev_dl,
+        checkpoint_prefix=arg("checkpoint-prefix"),
+        criterion=torch.nn.NLLLoss()
+    )
+
+    train(training_arguments)
