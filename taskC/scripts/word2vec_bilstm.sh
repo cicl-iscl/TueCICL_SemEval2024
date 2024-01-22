8bf9cf634de669ff478c89e75d5f6a9f6bf6c00c
waron97 <winkler.aron5@gmail.com>
2024-01-22 20:11:28 +0100

    Update word2vec_bilstm.sh script parameters

--- taskC/scripts/word2vec_bilstm.sh
+++ taskC/scripts/word2vec_bilstm.sh
@@ -7,10 +7,10 @@ python3 script.py train_word2vec_bilstm \
     --word2vec-bilstm-save-vocab ~/cicl/taskC/data/wiki2vec_vocab_500.pkl \
     --word2vec-bilstm-checkpoint-prefix word2vec-bilstm-test \
     --word2vec-bilstm-tokenizer-max-len 15000 \
-    --word2vec-bilstm-save-every-extended 1000 \
-    --word2vec-bilstm-save-every-pure 200 \
-    --word2vec-bilstm-epochs-extended 0 \
-    --word2vec-bilstm-epochs-pure 50 \
+    --word2vec-bilstm-save-every-extended 10 \
+    --word2vec-bilstm-save-every-pure 10 \
+    --word2vec-bilstm-epochs-extended 1 \
+    --word2vec-bilstm-epochs-pure 1 \
     --word2vec-bilstm-hidden-size 256 \
     --word2vec-bilstm-emb-size 16 \
     --word2vec-bilstm-num-layers 2 \
