6ae0c10b9a9ce1004f8295853499ff1c7f3cf094
Aron <winkler.aron5@gmail.com>
2024-01-22 15:41:46 +0100

    Update loss calculation in train_char_bilstm.py

--- taskC/src/trainers/train_char_bilstm.py
+++ taskC/src/trainers/train_char_bilstm.py
@@ -125,8 +125,8 @@ def train(args: TrainingArguments):
                 if i % args.save_every_pure == 0 and i != 0:
                     l = sum(losses) / len(losses)
                     losses = []
-                    best, latest = pt.for_steps(args.model, args.dev_loader, loss=l)
-                    pbar.set_postfix(best=best, latest=latest)
+                    best, latest = pt.for_steps(args.model, args.dev_loader)
+                    pbar.set_postfix(best=best, latest=latest, loss=l)
                     args.model.train()
         _e = epoch + args.epochs_extended
         pt.for_epoch(args.model, args.optimizer, _e, args.dev_loader)
