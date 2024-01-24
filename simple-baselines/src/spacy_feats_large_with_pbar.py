dc917448f5719fa313acb2881db9789ba31d6f3b
daniel stuhlinger <daniel@stuhlinger.eu>
2024-01-24 15:18:37 +0100

    mlp and spacy feats

--- simple-baselines/src/spacy_feats_large_with_pbar.py
+++ simple-baselines/src/spacy_feats_large_with_pbar.py
@@ -4,17 +4,21 @@ import lftk
 from load_data import get_data
 from tqdm import tqdm
 
+spacy.prefer_gpu()
 nlp = spacy.load("en_core_web_sm")
 
 train = pd.read_json("../../data/subtaskA_train_ppl.json")
-feats_df = pd.DataFrame()
-for i in tqdm(range(1198)):
-    docs = [nlp(doc) for doc in train.text.iloc[i:(i+1)*10]]
-    LFTK = lftk.Extractor(docs = docs)
-    feat_names = lftk.search_features(return_format="list_key")
+
+doc = nlp(train.text.iloc[1])
+LFTK = lftk.Extractor(docs=doc)
+feat_names = lftk.search_features(return_format="list_key")
+feats_df = pd.DataFrame(columns=feat_names)
+for i in tqdm(range(119757)):
+    doc = nlp(train.text.iloc[i])
+    LFTK = lftk.Extractor(docs = doc)
     extracted_features_train = LFTK.extract(features=feat_names)
-    batch = pd.DataFrame.from_dict(extracted_features_train)
-    pd.concat([feats_df, batch], ignore_index=True, axis=0)
+    batch = pd.Series(extracted_features_train)
+    feats_df.loc[i] = batch
 feats_df["log_ppl"] = train["log_ppl"]
 feats_df["id"] = train["id"]
 feats_df["label"] = train["label"]
