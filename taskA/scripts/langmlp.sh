3032fdf2bde79000ce3715c169805de3a0406ba1
waron97 <winkler.aron5gmail.com>
2024-01-29 16:47:28 +0100

    Update langmlp.sh and spacy.py scripts

--- taskA/scripts/langmlp.sh
+++ taskA/scripts/langmlp.sh
@@ -5,17 +5,18 @@ cd src
 python3 script.py train_lang_mlp \
     --lang-mlp-train \
     --lang-mlp-hidden-size 256 \
-    --lang-mlp-dropout 0.3 \
+    --lang-mlp-dropout 0.0 \
     --lang-mlp-lr 0.001 \
-    --lang-mlp-spacy-n-feats 68 \
-    --lang-mlp-spacy-train-feats ~/cicl/taskA/data/spacy/daniel_feats_train.jsonl \
-    --lang-mlp-spacy-dev-feats ~/cicl/taskA/data/spacy/daniel_feats_dev.jsonl \
+    --lang-mlp-spacy-n-feats 65 \
+    --lang-mlp-spacy-train-feats ~/cicl/taskA/data/spacy/spacy_feats_sm_train.jsonl \
+    --lang-mlp-spacy-dev-feats ~/cicl/taskA/data/spacy/spacy_feats_sm_dev.jsonl \
     --lang-mlp-ppl-train ~/cicl/taskA/data/spacy/daniel_ppl_train.json \
     --lang-mlp-ppl-dev ~/cicl/taskA/data/spacy/daniel_ppl_dev.json \
+    --lang-mlp-spacy-del-feats 26,27 \
     --lang-mlp-n-epochs 500 \
-    --lang-mlp-save-every 500 \
+    --lang-mlp-save-every 1 \
     --lang-mlp-checkpoint-prefix lang-mlp-test \
-    --lang-mlp-batch-size 64 \
+    --lang-mlp-batch-size 60000 \
     --lang-mlp-spacy-scale \
 
 
