6d4a8deec422c31d1190b9cfc4b0e42cbd05402b
Aron <winkler.aron5@gmail.com>
2024-02-03 10:18:17 +0100

    Update char_bilstm.sh script parameters

--- taskC/scripts/char_bilstm.sh
+++ taskC/scripts/char_bilstm.sh
@@ -4,16 +4,16 @@ cd src
 
 python3 script.py train_char_bilstm \
     --char-bilstm-tokenizer-path ~/cicl/taskC/data/charlm_vocab_uncondensed.pkl \
-    --char-bilstm-checkpoint-prefix char-bilstm-spacefix-xs \
-    --char-bilstm-tokenizer-max-len 7000 \
+    --char-bilstm-checkpoint-prefix char-bilstm-512-2-spacefix \
+    --char-bilstm-tokenizer-max-len 10000 \
     --char-bilstm-save-every-extended 2000 \
     --char-bilstm-save-every-pure 200 \
-    --char-bilstm-epochs-extended 0 \
+    --char-bilstm-epochs-extended 5 \
     --char-bilstm-epochs-pure 50 \
-    --char-bilstm-hidden-size 128 \
+    --char-bilstm-hidden-size 512 \
     --char-bilstm-emb-size 8 \
     --char-bilstm-num-layers 2 \
     --char-bilstm-dropout 0.2 \
     --char-bilstm-batch-size 8 \
     --char-bilstm-train \
-    --char-bilstm-prefer-cuda-device 1 \
+    --char-bilstm-prefer-cuda-device 0 \
